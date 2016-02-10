b60a5988f2aeb197729909f132dd62a63655ba7a
feizalbadri <feizal.asmoro@tokopedia.com>
2016-02-10 23:03:45 +0700

    STATIC_ROOT & STATICFILES_DIRS

--- headline/settings.py
+++ headline/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://static.getheadlineapp.com/'
+
+STATIC_ROOT = '/home/headline/webapps/static/'
 
 STATICFILES_DIRS = (
     '/home/headline/webapps/static/',
