8396634c324a2c360b8885b1a6ffb23601d6fc90
feizalbadri <feizal.asmoro@tokopedia.com>
2016-03-18 13:52:30 +0700

    remove duplicates

--- webservice/twitter/pull_data.py
+++ webservice/twitter/pull_data.py
@@ -130,6 +130,8 @@ def pull_latest_status(count=200):
 def remove_duplicate_news():
     for url in News.objects.values_list('url', flat=True).distinct():
         News.objects.filter(pk__in=News.objects.filter(url=url).values_list('id', flat=True)[1:]).delete()
+    for twitter_text in News.objects.values_list('twitter_text', flat=True).distinct():
+        News.objects.filter(pk__in=News.objects.filter(twitter_text=twitter_text).values_list('id', flat=True)[1:]).delete()
 
 
 def pull_title_and_images():
