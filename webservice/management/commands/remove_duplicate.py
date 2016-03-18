fcd45d73d003008e2cb5cb1cf882f1fa47a83f2e
feizalbadri <feizal.asmoro@tokopedia.com>
2016-03-18 13:50:58 +0700

    remove duplicates

--- webservice/management/commands/remove_duplicate.py
+++ webservice/management/commands/remove_duplicate.py
@@ -7,4 +7,7 @@ class Command(BaseCommand):
         for url in News.objects.values_list('url', flat=True).distinct():
             News.objects.filter(pk__in=News.objects.filter(url=url).values_list('id', flat=True)[1:]).delete()
             print url
+        for twitter_text in News.objects.values_list('twitter_text', flat=True).distinct():
+            News.objects.filter(pk__in=News.objects.filter(twitter_text=twitter_text).values_list('id', flat=True)[1:]).delete()
+            print url
         print News.objects.count()
