b5fe590ea6cb810d7730db6d496e7765c848b415
feizalbadri <feizal.asmoro@tokopedia.com>
2016-03-20 00:07:43 +0700

    add subscriber to bookmark & history resources

--- webservice/resources/History.py
+++ webservice/resources/History.py
@@ -2,10 +2,12 @@ from webservice.models import *
 from tastypie.serializers import Serializer
 from tastypie.resources import ModelResource, ALL_WITH_RELATIONS
 from webservice.resources.news import NewsResource
+from webservice.resources.subscriber import SubscriberResource
 from tastypie import fields
 
 
 class HistoryResource(ModelResource):
+    subscriber = fields.ForeignKey(SubscriberResource, 'subscriber', full=True)
     news = fields.ForeignKey(NewsResource, 'news', full=True)
 
     class Meta:
--- webservice/resources/bookmark.py
+++ webservice/resources/bookmark.py
@@ -2,10 +2,12 @@ from webservice.models import *
 from tastypie.serializers import Serializer
 from tastypie.resources import ModelResource, ALL_WITH_RELATIONS
 from webservice.resources.news import NewsResource
+from webservice.resources.subscriber import SubscriberResource
 from tastypie import fields
 
 
 class BookmarkResource(ModelResource):
+    subscriber = fields.ForeignKey(SubscriberResource, 'subscriber', full=True)
     news = fields.ForeignKey(NewsResource, 'news', full=True)
 
     class Meta:
