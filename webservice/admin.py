409ac605a710749c193f12174d34c713161b91a8
feizalbadri <feizal.asmoro@tokopedia.com>
2016-03-25 23:45:44 +0700

    add filter by category in channel admin

--- webservice/admin.py
+++ webservice/admin.py
@@ -51,6 +51,9 @@ class ChannelAdmin(admin.ModelAdmin):
                     'get_last_tweet_date')
     actions = [delete_all_news, delete_untitled_news]
     exclude = ('latest_news', 'subscriber')
+    list_filter = (
+        ('category', admin.RelatedOnlyFieldListFilter),
+    )
 
     def get_url(self, obj):
         return '<a href="%s" target="_blank">%s</a>' % (obj.url, obj.url)
