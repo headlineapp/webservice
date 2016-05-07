21bc89795c710d4e78ba12d19d911e77ea8fea62
feizalbadri <feizal.asmoro@tokopedia.com>
2016-05-08 02:46:09 +0700

    auth subscription

--- webservice/resources/subscription.py
+++ webservice/resources/subscription.py
@@ -5,7 +5,7 @@ from webservice.resources.user import UserResource
 from webservice.resources.channel import ChannelResource
 from tastypie.serializers import Serializer
 from tastypie.resources import ModelResource, ALL_WITH_RELATIONS
-from tastypie.authorization import Authorization, DjangoAuthorization
+from tastypie.authorization import Authorization
 
 
 class SubscriptionResource(ModelResource):
@@ -17,8 +17,7 @@ class SubscriptionResource(ModelResource):
         resource_name = 'subscription'
         serializer = Serializer(formats=['json'])
         always_return_data = True
-        allowed_methods = ['get', 'post']
-        authorization = DjangoAuthorization()
+        authorization = Authorization()
         filtering = {
             'user': ALL_WITH_RELATIONS,
             'channel': ALL_WITH_RELATIONS,
